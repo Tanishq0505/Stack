9aab65c0e8d52bf4dc3f8fa24562afdb5391aa06
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-11 13:21:13 +0530

    Create 17_Stack_implementation_using_array_and_declaring_stack_simply.c

---
+++ 17_Stack_implementation_using_array_and_declaring_stack_simply.c
@@ -0,0 +1,71 @@
+// Stack implementation using array and declaring stack simply (struct stack s;) in C (Explain in copy also) 
+
+#include <stdio.h>
+#include <stdlib.h>
+// https://www.youtube.com/watch?v=MD3mFgFwqBE&list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi&index=23&pp=iAQB
+// https://www.youtube.com/watch?v=r2yHEW8HmBE&list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi&index=24&pp=iAQB
+// see both video to understand you can see copy also
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack st)
+{
+    if (st.top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack st)
+{
+    if (st.top == st.size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    struct stack s;
+    s.size = 2;
+    s.top = -1;
+    s.arr = (int *)malloc(s.size * sizeof(int));
+
+    // Pushing an element manually
+    s.arr[0] = 7;
+    s.top++;
+    s.arr[1] = 5;
+    s.top++;
+    // Check if stack is empty
+    if (isEmpty(s))
+    {
+        printf("The stack is empty");
+    }
+    else
+    {
+        printf("The stack is not empty");
+    }
+    if (isFull(s))
+    {
+        printf("\n");
+        printf("The stack is full");
+    }
+    else
+    {
+        printf("\n");
+        printf("The stack is not full");
+    }
+    return 0;
+}
