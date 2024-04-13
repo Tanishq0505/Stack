36d990d826f3fcdcb2f76d38b08c87438f5aba6e
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-13 17:24:32 +0530

    Create 31_Asteroid_Collision.cpp

---
+++ 31_Asteroid_Collision.cpp
@@ -0,0 +1,43 @@
+// 31)Asteroid Collision (Leet code ) 
+    // https://youtu.be/7isfomHzzno 
+    // see video to undeerstand 
+//Using a stack<int> (T.C : O(n))
+class Solution {
+
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        
+        for(int &a : asteroids) {
+            
+            while(!st.empty() && a < 0 && st.top() > 0) {
+                int sum = a + st.top();
+                if(sum < 0) {
+                    st.pop();
+                } else if(sum > 0) {
+                    a = 0;
+                    break;
+                } else {
+                    st.pop();
+                    a = 0;
+                }
+            }
+            
+            if(a != 0)
+                st.push(a);
+            
+        }
+        
+        int s = st.size();
+        
+        vector<int> result(s);
+        int i = s-1;
+        while(!st.empty()) {
+            result[i] = st.top();
+            st.pop();
+            i--;
+        }
+        
+        return result;
+    }
+};
