ca3e9b1c6ec9ef1585324259835be7c35fc49997
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-16 01:57:22 +0530

    Create 33_Online_Stock_Span.cpp

---
+++ 33_Online_Stock_Span.cpp
@@ -0,0 +1,31 @@
+// 901. Online Stock Span (Leet code ) 
+
+// https://youtu.be/jtOWh4pfmYM 
+// see video to understand 
+class StockSpanner {
+public:
+
+    stack<pair<int,int>>st; // price, span
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        
+        while(!st.empty() && st.top().first <= price){
+            span += st.top().second;
+            st.pop();
+        }
+        
+        st.push({price, span});
+        return span;
+    }
+};
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
